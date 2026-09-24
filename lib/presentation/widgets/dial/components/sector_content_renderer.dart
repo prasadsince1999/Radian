722845e03f8be9a5014c1c8b0dd8c86a52bda0ba
K. PRASAD RAO <kpr25121999@gmail.com>
2026-09-24 13:04:08 -0700

    fix(dial): restore sector renderer and only place chips that fit

--- lib/presentation/widgets/dial/components/sector_content_renderer.dart
+++ lib/presentation/widgets/dial/components/sector_content_renderer.dart
@@ -1,1 +1,9 @@
-PLACEHOLDER
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+import '../../../../core/constants/app_layout_constants.dart';
+import '../../../../core/geometry/sector_math.dart';
+import '../../../../core/utils/time_formatters.dart';
+import '../../../../domain/models/sector_event.dart';
+import 'sector_pill_renderer.dart';
